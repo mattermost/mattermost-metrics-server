c024c8606c100c44b8490d71670bd56e6504c660
Saturnino Abril <saturnino.abril@gmail.com>
2019-05-02 20:13:10 +0800

    add 'GET traces/{device_unique_id}' endpoint, sql script, sample raw metric data, and update data fields (#3)

--- src/get_metrics.js
+++ src/get_metrics.js
@@ -3,7 +3,12 @@
 
 const {Client} = require('pg');
 
-import {errorResponse, successResponse, runWarm} from './utils';
+import {
+    errorResponse,
+    getQueryParams,
+    successResponse,
+    runWarm,
+} from './utils';
 
 // Reuse DB connection
 if (typeof client === 'undefined') {
@@ -18,8 +23,16 @@ const getMetrics = (event, context, callback) => {
     // Allows to freeze open connections to a database
     context.callbackWaitsForEmptyEventLoop = false;
 
+    const {
+        page,
+        perPage,
+    } = getQueryParams(event.queryStringParameters);
+    
+    const limitQuery = `LIMIT ${perPage}`;
+    const offsetQuery = `OFFSET ${page * perPage}`;
+
     client.query(
-        'SELECT * FROM metrics ORDER BY id DESC LIMIT 100;',
+        `SELECT * FROM metrics ORDER BY id DESC ${limitQuery} ${offsetQuery};`,
         (err, res) => {
             let response;
             if (err) {
